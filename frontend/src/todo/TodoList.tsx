4155631303a279dadce393d7e66568574e738cd9
ytanaka44 <y.tanaka@idein.jp>
2023-12-10 10:03:46 +0900

    Enable checking functionality for task list checkboxes(#10)

--- frontend/src/todo/TodoList.tsx
+++ frontend/src/todo/TodoList.tsx
@@ -12,7 +12,7 @@ import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
 import CheckCircleOutlinedIcon from "@mui/icons-material/CheckCircleOutlined";
 import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 import StarIcon from "@mui/icons-material/Star";
-import { getAllTodos } from "./api/api";
+import { getAllTodos, updateTodo } from "./api/api";
 import { TodoState } from "./types/types";
 import AddIcon from "@mui/icons-material/Add";
 import AddTodo from "./AddTodo";
@@ -23,6 +23,20 @@ const TodoList: React.FC = () => {
   const fetchTodos = async () => {
     const todos = await getAllTodos();
     setDatas(todos);
+  };
+
+  const handleCheckDone = async (data: TodoState) => {
+    const updateData = { ...data, done: !data.done };
+    await updateTodo(data.id, updateData);
+
+    fetchTodos();
+  };
+
+  const handleCheckImportant = async (data: TodoState) => {
+    const updateData = { ...data, important: !data.important };
+    await updateTodo(data.id, updateData);
+
+    fetchTodos();
   };
 
   useEffect(() => {
@@ -44,43 +58,47 @@ const TodoList: React.FC = () => {
           Tasks
         </Typography>
         <Box sx={{ overflowY: "auto", maxHeight: "75vh" }}>
-          {datas.map((data, index) => (
-            <Box key={data.id} sx={{ backgroundColor: "lightgray", mb: 1 }}>
-              <Box
-                key={index}
-                sx={{
-                  display: "flex",
-                  p: 2,
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                }}
-              >
-                <Box sx={{ display: "flex", alignItems: "center" }}>
-                  <Checkbox
-                    icon={<CircleOutlinedIcon />}
-                    checkedIcon={<CheckCircleOutlinedIcon />}
-                    checked={data.done}
-                  />
-                  <Box
-                    sx={{
-                      pl: 2,
-                      display: "flex",
-                      flexDirection: "column",
-                      alignItems: "flex-start",
-                    }}
-                  >
-                    <Box>{data.title}</Box>
-                    <Box>{data.date}</Box>
+          {datas
+            .filter((data) => !data.done)
+            .map((data, index) => (
+              <Box key={data.id} sx={{ backgroundColor: "lightgray", mb: 1 }}>
+                <Box
+                  key={index}
+                  sx={{
+                    display: "flex",
+                    p: 2,
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <Checkbox
+                      icon={<CircleOutlinedIcon />}
+                      checkedIcon={<CheckCircleOutlinedIcon />}
+                      checked={data.done}
+                      onChange={() => handleCheckDone(data)}
+                    />
+                    <Box
+                      sx={{
+                        pl: 2,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                      }}
+                    >
+                      <Box>{data.title}</Box>
+                      <Box>{data.date}</Box>
+                    </Box>
                   </Box>
+                  <Checkbox
+                    icon={<StarBorderOutlinedIcon />}
+                    checkedIcon={<StarIcon />}
+                    checked={data.important}
+                    onChange={() => handleCheckImportant(data)}
+                  />
                 </Box>
-                <Checkbox
-                  icon={<StarBorderOutlinedIcon />}
-                  checkedIcon={<StarIcon />}
-                  checked={data.important}
-                />
               </Box>
-            </Box>
-          ))}
+            ))}
         </Box>
       </Box>
       <AddTodo fetchTodos={fetchTodos} />
