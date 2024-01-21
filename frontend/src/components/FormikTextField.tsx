a6294becef73419e36174a167007bb3c5a367312
ytanaka44 <y.tanaka@idein.jp>
2024-01-22 06:23:00 +0900

    fix: CustomTextFieldを修正(#39)

---
+++ frontend/src/components/FormikTextField.tsx
@@ -0,0 +1,39 @@
+import { TextField } from "@mui/material";
+import { FormikProps } from "formik";
+import React from "react";
+
+interface FormikTextFieldProps<T> {
+  name: keyof T;
+  label: string;
+  formik: FormikProps<T>;
+  variant?: "outlined" | "filled" | "standard";
+  type?: "text" | "password";
+}
+
+// ジェネリック型をコンポーネントに適用
+export const FormikTextField = <T extends {}>(
+  props: FormikTextFieldProps<T>
+) => {
+  return (
+    <TextField
+      size="small"
+      id={String(props.name)}
+      name={String(props.name)}
+      label={String(props.label)}
+      type={String(props.type)}
+      variant={props.variant}
+      fullWidth
+      value={props.formik.values[props.name]}
+      onChange={props.formik.handleChange}
+      onBlur={props.formik.handleBlur}
+      error={
+        !!(props.formik.touched[props.name] && props.formik.errors[props.name])
+      }
+      helperText={String(
+        props.formik.touched[props.name] && props.formik.errors[props.name]
+          ? props.formik.errors[props.name]
+          : ""
+      )}
+    />
+  );
+};
